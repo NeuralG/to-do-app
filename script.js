ba17abeee72612c2aad946236ab2ba0bf996c6cc
Umutcan Çetin <66206623+NeuralG@users.noreply.github.com>
2023-10-26 21:41:08 +0300

    app

---
+++ script.js
@@ -0,0 +1,48 @@
+function main(){
+
+    const inputEl = document.getElementById("input")
+    const buttonEl = document.getElementById("submit")
+    const listEl = document.getElementById("list")
+    const deleteEl = document.getElementById("delete")
+    
+    let myItems = []
+
+    let count = 0
+
+    function addToList(count,value){
+        localStorage.setItem(count,value);
+        fetchItems()
+    }
+
+    function render(){
+        listEl.innerHTML = ""
+        for (let i=0;i<myItems.length;i++){
+            listEl.innerHTML += `<li>${myItems[i]}</li>`
+        }
+    }
+
+    function fetchItems(){
+        count = localStorage.length
+        myItems = []
+        for (let i=0;i<count;i++){
+            let item = localStorage.getItem(`${i}`)
+            myItems.push(item)
+        }
+        render()
+    }
+
+    buttonEl.addEventListener("click", function(){
+        let myValue = inputEl.value
+        if (myValue){
+            addToList(count,myValue)
+            inputEl.value = ""
+        }
+    })
+
+    deleteEl.addEventListener("click", function(){
+        localStorage.clear()
+        location.reload()
+    })
+    
+    fetchItems()
+}
